6656d8b216408a3523fc3038986e986831031b6f
Computer-hp <windowsc045@gmail.com>
2024-05-05 10:54:59 +0200

    modified

---
+++ BluethootChess/WinFormsApp1/WinFormsApp1/ValueTupleExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    public static class ValueTupleExtensions
+    {
+        public static int X(this (int, int) tuple) => tuple.Item1;
+        public static int Y(this (int, int) tuple) => tuple.Item2;
+    }
+}
